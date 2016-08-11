252002c4c2303333fcb1b92188041b15392be65f
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2016-08-11 15:37:09 +0300

    Add src and test file and travis config

---
+++ src/Gateway.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Checkout.fi driver for Omnipay PHP payment processing library
+ * https://www.checkout.fi/
+ *
+ * Latest driver release:
+ * https://github.com/TODO/
+ *
+ */
+namespace Omnipay\CheckoutFi;
+
---
+++ tests/GatewayTest.php
@@ -0,0 +1,4 @@
+<?php
+
+namespace Omnipay\CheckoutFi;
+
