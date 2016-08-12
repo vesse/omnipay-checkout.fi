bf0825ba0027b5b881bc07cf98eec8d8fa948116
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2016-08-12 11:08:30 +0300

    Move purchase request specific methods away from abstract request

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -10,7 +10,6 @@ use \Omnipay\Common\Message\AbstractRequest as BaseAbstractRequest;
 
 abstract class AbstractRequest extends BaseAbstractRequest
 {
-    // TODO: Not all of these are common for requests, most need to be moved to PurchaseRequest
     public function getPaymentUrl()
     {
         return $this->getParameter('paymentUrl');
@@ -89,115 +88,5 @@ abstract class AbstractRequest extends BaseAbstractRequest
     public function setReference($reference)
     {
         $this->setParameter('reference', $reference);
-    }
-
-    public function getMessage()
-    {
-        return $this->getParameter('message');
-    }
-
-    public function setMessage($message)
-    {
-        $this->setParameter('message', $message);
-    }
-
-    public function getLanguage()
-    {
-        return $this->getParameter('language');
-    }
-
-    public function setLanguage($language)
-    {
-        $this->setParameter('language', $language);
-    }
-
-    public function getCountry()
-    {
-        return $this->getParameter('country');
-    }
-
-    public function setCountry($country)
-    {
-        $this->setParameter('country', $country);
-    }
-
-    public function getDeliveryDate()
-    {
-        return $this->getParameter('deliveryDate');
-    }
-
-    public function setDeliveryDate($deliveryDate)
-    {
-        $this->setParameter('deliveryDate', $deliveryDate);
-    }
-
-    public function getFirstName()
-    {
-        return $this->getParameter('firstName');
-    }
-
-    public function setFirstName($firstName)
-    {
-        $this->setParameter('firstName', $firstName);
-    }
-
-    public function getFamilyName()
-    {
-        return $this->getParameter('familyName');
-    }
-
-    public function setFamilyName($familyName)
-    {
-        $this->setParameter('familyName', $familyName);
-    }
-
-    public function getAddress()
-    {
-        return $this->getParameter('address');
-    }
-
-    public function setAddress($address)
-    {
-        $this->setParameter('address', $address);
-    }
-
-    public function getPostcode()
-    {
-        return $this->getParameter('postcode');
-    }
-
-    public function setPostcode($postcode)
-    {
-        $this->setParameter('postcode', $postcode);
-    }
-
-    public function getPostoffice()
-    {
-        return $this->getParameter('postoffice');
-    }
-
-    public function setPostoffice($postoffice)
-    {
-        $this->setParameter('postoffice', $postoffice);
-    }
-
-    public function getEmail()
-    {
-        return $this->getParameter('email');
-    }
-
-    public function setEmail($email)
-    {
-        $this->setParameter('email', $email);
-    }
-
-    public function getPhone()
-    {
-        return $this->getParameter('phone');
-    }
-
-    public function setPhone($phone)
-    {
-        $this->setParameter('phone', $phone);
     }
 }
--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -58,4 +58,114 @@ class PurchaseRequest extends AbstractRequest
     {
         return $this->httpClient->request('POST', $this->getPaymentUrl(), ['form_params' => $data]);
     }
+
+    public function getMessage()
+    {
+        return $this->getParameter('message');
+    }
+
+    public function setMessage($message)
+    {
+        $this->setParameter('message', $message);
+    }
+
+    public function getLanguage()
+    {
+        return $this->getParameter('language');
+    }
+
+    public function setLanguage($language)
+    {
+        $this->setParameter('language', $language);
+    }
+
+    public function getCountry()
+    {
+        return $this->getParameter('country');
+    }
+
+    public function setCountry($country)
+    {
+        $this->setParameter('country', $country);
+    }
+
+    public function getDeliveryDate()
+    {
+        return $this->getParameter('deliveryDate');
+    }
+
+    public function setDeliveryDate($deliveryDate)
+    {
+        $this->setParameter('deliveryDate', $deliveryDate);
+    }
+
+    public function getFirstName()
+    {
+        return $this->getParameter('firstName');
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->setParameter('firstName', $firstName);
+    }
+
+    public function getFamilyName()
+    {
+        return $this->getParameter('familyName');
+    }
+
+    public function setFamilyName($familyName)
+    {
+        $this->setParameter('familyName', $familyName);
+    }
+
+    public function getAddress()
+    {
+        return $this->getParameter('address');
+    }
+
+    public function setAddress($address)
+    {
+        $this->setParameter('address', $address);
+    }
+
+    public function getPostcode()
+    {
+        return $this->getParameter('postcode');
+    }
+
+    public function setPostcode($postcode)
+    {
+        $this->setParameter('postcode', $postcode);
+    }
+
+    public function getPostoffice()
+    {
+        return $this->getParameter('postoffice');
+    }
+
+    public function setPostoffice($postoffice)
+    {
+        $this->setParameter('postoffice', $postoffice);
+    }
+
+    public function getEmail()
+    {
+        return $this->getParameter('email');
+    }
+
+    public function setEmail($email)
+    {
+        $this->setParameter('email', $email);
+    }
+
+    public function getPhone()
+    {
+        return $this->getParameter('phone');
+    }
+
+    public function setPhone($phone)
+    {
+        $this->setParameter('phone', $phone);
+    }
 }
