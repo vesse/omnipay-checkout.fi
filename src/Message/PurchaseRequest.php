1797dc3f7bc6fa2c8d212dbdabc9b76097b9c080
Olli Jaakkola <olli.jaakkola@leonidasoy.fi>
2016-08-12 10:50:43 +0300

    Fix syntax errors

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -11,44 +11,45 @@ namespace Omnipay\CheckoutFi\Message;
 class PurchaseRequest extends AbstractRequest
 {
     private static $MAC_FIELDS = array(
-        'VERSION', 'STAMP', 'AMOUNT', 'REFERENCE', 'MESSAGE',
-        'LANGUAGE', 'MERCHANT', 'RETURN', 'CANCEL', 'REJECT',
-        'DELAYED', 'COUNTRY', 'CURRENCY', 'DEVICE', 'CONTENT',
-        'TYPE', 'ALGORITHM', 'DELIVERY_DATE', 'FIRSTNAME', 'FAMILYNAME',
-        'ADDRESS', 'POSTCODE', 'POSTOFFICE');
+        'VERSION', 'STAMP', 'AMOUNT', 'REFERENCE', 'MESSAGE',
+        'LANGUAGE', 'MERCHANT', 'RETURN', 'CANCEL', 'REJECT',
+        'DELAYED', 'COUNTRY', 'CURRENCY', 'DEVICE', 'CONTENT',
+        'TYPE', 'ALGORITHM', 'DELIVERY_DATE', 'FIRSTNAME', 'FAMILYNAME',
+        'ADDRESS', 'POSTCODE', 'POSTOFFICE');
 
     public function getData()
     {
         $data = array(
-            'VERSION'       = $this->getVersion(),
-            'STAMP'         = $this->getStamp(),
-            'AMOUNT'        = $this->getAmount(),
-            'REFERENCE'     = $this->getReference(),
-            'MESSAGE'       = $this->getMessage(),
-            'LANGUAGE'      = $this->getLanguage(),
-            'MERCHANT'      = $this->getMerchant(),
-            'RETURN'        = $this->getReturnUrl(),
-            'CANCEL'        = $this->getReturnUrl(),
-            'REJECT'        = $this->getReturnUrl(),
-            'DELAYED'       = $this->getReturnUrl(),
-            'COUNTRY'       = $this->getCountry(),
-            'CURRENCY'      = 'EUR',
-            'DEVICE'        = '1',
-            'CONTENT'       = '1',
-            'TYPE'          = '0',
-            'ALGORITHM'     = '3',
-            'DELIVERY_DATE' = $this->getDeliveryDate(),
-            'FIRSTNAME'     = $this->getFirstName(),
-            'FAMILYNAME'    = $this->getFamilyName(),
-            'ADDRESS'       = $this->getAddress(),
-            'POSTCODE'      = $this->getPostcode(),
-            'POSTOFFICE'    = $this->getPostoffice(),
-            'EMAIL'         = $this->getEmail(),
-            'PHONE'         = $this->getPhone()
+            'VERSION'       => $this->getVersion(),
+            'STAMP'         => $this->getStamp(),
+            'AMOUNT'        => $this->getAmount(),
+            'REFERENCE'     => $this->getReference(),
+            'MESSAGE'       => $this->getMessage(),
+            'LANGUAGE'      => $this->getLanguage(),
+            'MERCHANT'      => $this->getMerchant(),
+            'RETURN'        => $this->getReturnUrl(),
+            'CANCEL'        => $this->getReturnUrl(),
+            'REJECT'        => $this->getReturnUrl(),
+            'DELAYED'       => $this->getReturnUrl(),
+            'COUNTRY'       => $this->getCountry(),
+            'CURRENCY'      => 'EUR',
+            'DEVICE'        => '1',
+            'CONTENT'       => '1',
+            'TYPE'          => '0',
+            'ALGORITHM'     => '3',
+            'DELIVERY_DATE' => $this->getDeliveryDate(),
+            'FIRSTNAME'     => $this->getFirstName(),
+            'FAMILYNAME'    => $this->getFamilyName(),
+            'ADDRESS'       => $this->getAddress(),
+            'POSTCODE'      => $this->getPostcode(),
+            'POSTOFFICE'    => $this->getPostoffice(),
+            'EMAIL'         => $this->getEmail(),
+            'PHONE'         => $this->getPhone()
         );
 
-        $hashString = join('+', array_map(function($field) use (&$data) { return $data[$field]; }, self::$MAC_FIELDS))
-            . '+' . $this->getMerchantSecret();
+        $hashString = join('+', array_map(function ($field) use (&$data) {
+            return $data[$field];
+        }, self::$MAC_FIELDS)) . '+' . $this->getMerchantSecret();
         $data['MAC'] = strtoupper(md5($hashString));
         return $data;
     }
